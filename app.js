475b88069c7a57aa17ca1bb59994aa391757b967
Chu Ngọc Việt Đức <23110016@student.hcmute.edu.vn>
2026-04-10 23:48:24 +0700

    khoi tao app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 import express from 'express';
+
+const   app = express();
+app.use(express.json());
+const port = 3000;
+
+
+app.listen(port, () => console.log(` Server đang chạy tại http://localhost:${port}`));
